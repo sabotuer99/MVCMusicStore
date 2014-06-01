bf2e0f762038f19764827c2a62b55bf36304de81
Troy Whorten <sabotuer99@gmail.com>
2014-05-31 23:37:03 -0600

    Tutorial thru page 40

---
+++ MVCMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCMusicStore.Models;
+
+namespace MVCMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+        public ActionResult Index()
+        {
+            //return Content("Hello from Store.Index()"); 
+
+            var genres = new List<Genre>
+            {
+                new Genre {Name = "Disco"},
+                new Genre {Name = "Jazz"},
+                new Genre {Name = "Rock"}
+            };
+
+            return View(genres);
+        }
+
+        //
+        // GET: /Store/Browse?Genre=blah
+        public ActionResult Browse(string genre)
+        {
+            //string message = HttpUtility.HtmlEncode("Store.Browse, Genre = " + genre);
+            //return Content(message); 
+
+            var genreModel = new Genre { Name = genre };
+            return View(genreModel);
+        }
+
+        //
+        // GET: /Store/Details/5
+        public ActionResult Details(int id)
+        {
+            //string message = "Store.Details, ID = " + id;
+            //return Content(message); 
+
+            var album = new Album { Title = "Album" + id };
+            return View(album);
+        }
+
+	}
+}
